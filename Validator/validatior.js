c5ad6f7773bd41dc29e1abbddc0f0820a5b92862
ShermanDrake <shermandrake@comcast.net>
2015-09-08 17:43:29 -0600

    third commit

---
+++ Validator/validatior.js
@@ -0,0 +1,14 @@
+var name = prompt("What's your name?");
+var food= prompt("What's for lunch?");
+
+var	myString = name  + food
+
+
+console.log( 'You entered: ' + name + ', ' + food )
+console.log( 'There are ' + myString.length + ' characters in the string.')
+console.log( 'The third character is ' + myString.charAt(3) )
+console.log( 'There are ' + myString.length + ' characters in the string.')
+console.log( 'Lowercase: ' + myString.toLowerCase())
+console.log( 'Lowercase: ' + myString.toUpperCase())
+console.log( 'Sentance: ' + name + ', had ' + food + ' or lunch.')
+console.log( 'Subword: ' + myString.charAt(2, 3, 4) )
---
+++ Validator/validator.js
@@ -0,0 +1,25 @@
+var phoneNumber = prompt("please enter your phone number.")
+
+
+
+
+
+
+
+
+console.log('You entered: ' + phoneNumber )
+
+// var phoneNumber = prompt("Please enter your phone number"());
+// var food = prompt("What's for lunch?");
+
+// var	myString = name  + food
+
+
+// console.log( 'You entered: ' + phoneNumber + ', ' + food )
+// console.log( 'There are ' + myString.length + ' characters in the string.')
+// console.log( 'The third character is ' + myString.charAt(3) )
+// console.log( 'There are ' + myString.length + ' characters in the string.')
+// console.log( 'Lowercase: ' + myString.toLowerCase())
+// console.log( 'Lowercase: ' + myString.toUpperCase())
+// console.log( 'Sentance: ' + phoneNumber + ', had ' + food + ' or lunch.')
+// console.log( 'Subword: ' + myString.charAt(2, 3, 4) )
