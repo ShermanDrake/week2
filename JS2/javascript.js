bdd6ffb99375d264d5619bbaad4c9122fc201795
ShermanDrake <shermandrake@comcast.net>
2015-09-08 13:30:34 -0600

    second commit

--- JS2/javascript.js
+++ JS2/javascript.js
@@ -1,33 +1,59 @@
-//var user_name = prompt('what is your name?' , 'type it here.');
-//document.write('Hello ' + user_name + '. Welcome to my page!');
+var name = prompt("What's your name?");
+var food= prompt("What's for lunch?");
 
-//for(var i = 0; i <= 500; i+=100) {
-//	console.log(i);
+var	myString = name  + food
 
-//}
 
-// for(var i = 1; i <= 64; i=i*2) {
-// 	console.log(i);
+console.log( 'You entered: ' + name + ', ' + food )
+console.log( 'There are ' + myString.length + ' characters in the string.')
+console.log( 'The third character is ' + myString.charAt(3) )
+console.log( 'There are ' + myString.length + ' characters in the string.')
+console.log( 'Lowercase: ' + myString.toLowerCase())
+console.log( 'Lowercase: ' + myString.toUpperCase())
+console.log( 'Sentance: ' + name + ', had ' + food + ' or lunch.')
+console.log( 'Subword: ' + myString.charAt(2, 3, 4) )
 
-// }
 
-// for(var i = 0; i <= 10; i=i+2) {
-// console.log(i);
 
-// }
 
-// for(var i = 3; i <= 15; i=i+3) {
-// console.log(i);
 
-// }
 
-// for(var i = 9; i >= 0; i=i-1) {
-// console.log(i);
 
-// }
 
-for(var i = 0; i <= 3; i=i+1) {
-console.log(i);
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
