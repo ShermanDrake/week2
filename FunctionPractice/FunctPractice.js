cd5ad5d345d7048acbd3cd07595d17748a37a894
ShermanDrake <shermandrake@comcast.net>
2015-09-11 14:22:58 -0600

    third commit

--- FunctionPractice/FunctPractice.js
+++ FunctionPractice/FunctPractice.js
@@ -1,7 +1,161 @@
-var tripleFive = function()
+1.
+var tripleFive = function() {
 
-for (var i=0; i < 3; i++) {
-	console.log(tripleFive)
+for(var i=0; i < 3; i++) {
 
-};
+	console.log("Five!")
+	console.log("\n")
+
+	}
+
+}
+tripleFive()
+
+
+// 2.
+// function lastLetter(str) {
+	
+// 	var res = str.charAt(str.length-1);
+
+// 	console.log(res)
+
+// }
+
+// lastLetter("hello")
+// lastLetter("island")
+// lastLetter("refactoru")
+
+
+
+// 3.
+// function square(num) {
+
+	
+// 	console.log(num * num)
+
+// }
+// square(3)
+// square(5)
+
+
+
+// 4.
+// function negate(num) {
+
+	
+// 	console.log(num - (num * 2))
+
+// }
+// negate(5)
+// negate(-8)
+
+
+
+// 5.
+// var toArray = function(num1, num2, num3) {
+
+// 	console.log(num1, num2, num3)
+
+// }
+// 	toArray(1, 4, 5)
+// 	toArray(8, 9, 10)
+
+
+
+// 6.
+// var startsWithA = function() {
+// 	var word = prompt("enter a word");
+// 	console.log(typeof word);
+// 	word = word.toUpperCase();
+// 	var startsWith = word.charAt(0);
+// 		if (startsWith === "A") {
+// 			return true;
+// 		}
+
+// }
+
+// console.log(startsWithA());
+
+
+
+
+// 7.
+// var excite = function (){
+// 	var word = prompt("enter a word");
+// 	return word + "!!!";
+// }
+// console.log(excite());
+
+
+
+// 8.
+// var sun = function() {
+// 	var word = prompt("enter a word")
+// 	if (word.toUpperCase().indexOf("SUN") >= 0) {
+
+// 			return true;
+// 	} else {
+// 			return false;
+// 	}
+
+
+// }
+
+// console.log(sun())
+
+// // sun("sundries")
+// // sun("asunder")
+// // sun("catapult")
+
+
+
+// 9.
+// var tiny = function() {
+// 	var number = prompt('enter a number');
+
+// 	return (number > 0 && number < 1);
+
+// }
+
+// console.log(tiny());
+
+
+
+// 10.
+// var getSeconds = function() {
+
+// 	var time = prompt('enter time');
+
+// 	timeArray = time.split(":");
+
+// 	return (timeArray[0]*60 + (+timeArray[1]) + " seconds");
+
+// }
+
+// console.log(getSeconds())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
